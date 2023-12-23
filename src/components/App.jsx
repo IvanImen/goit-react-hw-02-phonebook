2000bbff857011f2a1f38ff73507563bfbe39548
IvanImen <vanyagarret@gmail.com>
2023-12-23 02:18:32 +0200

    task2_commit1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = e => {
+    console.log('e.target.value  :>> ', e.target.value);
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState(prev => ({
+      contacts: [...prev.contacts, { name: this.state.name, id: nanoid() }],
+    }));
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+    );
+  }
+}
