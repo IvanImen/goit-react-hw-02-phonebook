3da02adda8888387c52133a39104a8e79fc493d9
IvanImen <vanyagarret@gmail.com>
2023-12-30 00:28:49 +0200

    task2-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
 
   handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value.trim() });
+    this.setState({
+      [name]: name === 'filter' ? value.trim().toLowerCase() : value.trim(),
+    });
   };
 
   handleSubmit = e => {
@@ -26,6 +34,13 @@ export class App extends Component {
   };
 
   render() {
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter);
+    });
+    console.log('filteredContacts :>> ', filteredContacts);
+
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -45,8 +60,16 @@ export class App extends Component {
           />
           <button type="submit">Add contact</button>
         </form>
+
+        <input
+          type="text"
+          name="filter"
+          onChange={this.handleChange}
+          value={this.state.filter}
+        />
+
         <ul>
-          {this.state.contacts.map(({ id, name, number }) => {
+          {filteredContacts.map(({ id, name, number }) => {
             return (
               <li key={id}>
                 <p>{name} </p>
